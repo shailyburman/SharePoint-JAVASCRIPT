ea6413dada6b40acf5d8309399f4362c466516ba
KM Sonia <shailyburman9@gmail.com>
2025-01-03 15:31:27 +0530

    Hello

---
+++ 2-JAN-55-ss.js
@@ -0,0 +1,17 @@
+// what is javascript.
+// javascript is a server-side and client side scripting and object oriented langauge uased create web application
+// also used to create andriod app using JS Library.
+
+//Features.
+//1. server-side(backend) and client side(frontend) supportive.
+//2. Easily integrated with HTML-CSS.
+//3. Dynamically type langauge.
+//4. Event-driven programming.
+//5. synchronous(SYNC) and asynchronous(ASYNC).
+//6. By defualt single threaded and also used as multithreaded.
+//7. Top Library : Reactjs , Vuejs , angularjs , electronjs.
+
+//type of appling JS 
+//1. internal js
+//2. external js .
+
